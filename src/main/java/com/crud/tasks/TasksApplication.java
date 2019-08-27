7104ba0a0aa33886de8574f2761a3456a281bd54
SebastianS85 <smuk.sebastian@gmx.de>
2019-08-27 17:40:26 +0200

    14.3

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -10,8 +10,6 @@ public class TasksApplication {
 	public static void main(String[] args) {
 
 
-
-
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -2,11 +2,13 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class TaskDto {
-    private long id;
+    private Long id;
     private String title;
     private String content;
 }
